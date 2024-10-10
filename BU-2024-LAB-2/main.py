04571308e979b3a48ed88776255683d5cb400a62
Daniel Baldwin (s5713731) <drqonic@protonmail.com>
2024-10-10 14:38:48 +0100

    Added new feature to reliably get a real number and get a select digit from it.

--- BU-2024-LAB-2/main.py
+++ BU-2024-LAB-2/main.py
@@ -22,3 +22,38 @@ print(f"{greeting_string[0]}{greeting_string[4]}")
 print(f"{greeting_string[0:5]}")
 print(f"{greeting_string[-1]}")
 print(f"{greeting_string[-7:-4]}") # -7 will start at the 7th last character, and -3 will get all characters 3rd from last.
+
+while True:
+    try:
+        number = input("Number: ")
+
+        cast_number = int(number)
+    except ValueError:
+        continue
+    
+    break
+
+while True:
+    try:
+        digit_index = input("Digit Index: ")
+
+        cast_digit_index = int(digit_index)
+    except ValueError:
+        continue
+    
+    break
+
+real_digits = 0
+
+for digit in number:
+    try:
+        is_int = int(digit)
+
+        real_digits += 1
+
+        if real_digits == cast_digit_index:
+            print(f"First digit of {number} is {digit}")
+
+            break
+    except ValueError:
+        continue
