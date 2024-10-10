a0ddf425c31070073f70e4522a69b92b58c171ff
Daniel Baldwin (s5713731) <drqonic@protonmail.com>
2024-10-10 14:19:36 +0100

    Reorganized everything

---
+++ BU-2024-LAB-2/main.py
@@ -0,0 +1,24 @@
+a = 11
+a += 5
+a -= 5
+a *= 5
+a /= 5
+a //= 5
+a **= 5
+a %= 5
+
+b = 5
+
+print(f"Value of 'a'={a}, type of 'a'={type(a)}")
+print(f"a < b={a < b}")
+
+unit_name = "Level 4 Programming"
+
+print(f"{unit_name[0]}{unit_name[3]}{unit_name[8]}")
+
+greeting_string = "Welcome to Programming unit! This is level 4 unit :)"
+
+print(f"{greeting_string[0]}{greeting_string[4]}")
+print(f"{greeting_string[0:5]}")
+print(f"{greeting_string[-1]}")
+print(f"{greeting_string[-7:-4]}")
