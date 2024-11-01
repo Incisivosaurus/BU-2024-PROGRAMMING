f62b0f7d6fa0d5024c6f374c162cc8fbb42619ab
Incisivosaurus <drqonic@protonmail.com>
2024-11-01 09:11:57 +0000

    Created daily_sales function used to handle product sales and storing records of said sales

--- IMS/sales.py
+++ IMS/sales.py
@@ -1,5 +1,6 @@
 import random
 
+
 def daily_sales(available_items, inventory_records, current_day):
     '''
 ***********COMPLETE THIS FUNCTION***********
@@ -19,5 +20,18 @@ available_items:(integer) This function returns this integer which updates the a
 The function will also update the inventory_records (For restocking) for a  given current day. 
 
     '''
+    restock_interval = 7 # How many days must be waited before restocking inventory
+
+    # When it is restock day, the shop will be closed, thus we return early as we aren't selling anything.
+    if current_day % restock_interval == 0:
+        return available_items
+
+    # Generate a random value from 0 to 200 to simulate sold units
+    sold_units = random.randint(0, min(200, available_items)) # Using min will prevent us from selling more shirts than we have in stock
+    available_items -= sold_units # Update the amount of available items after sales
+
+    # Update the latest entries data
+    inventory_records[-1][1] = sold_units # Update sold units to be the new accurate amount
+    inventory_records[-1][3] = available_items # Update the new amount of available items
     
     return available_items
