f775ef27e9651638cc1b567730e04d58bdaf7d05
Incisivosaurus <drqonic@protonmail.com>
2024-11-14 14:20:10 +0000

    Added comments to max of three finder

--- BU-2024-LAB-4/max_of_three.py
+++ BU-2024-LAB-4/max_of_three.py
@@ -13,12 +13,12 @@ def max_of_three(num1, num2, num3):
     # Hint: you are required to make use of maximum variable that is returned by the function below.
     # Complete your code implementation here...
 
-    biggest = num1
+    biggest = num1 # Make a default biggest number set to num1
 
     if num2 > biggest and num2 >= num3:
-        biggest = num2
+        biggest = num2 # If num2 is bigger than the current biggest (num1) and bigger than or equal to num3, then this is the new biggest
     elif num3 > biggest and num3 >= num2:
-        biggest = num3
+        biggest = num3 # If num3 is bigger than the current biggest (num1) and bigger than or equal to num2, then this is the new biggest
 
     # We could just do max(num1, num2, num3) but we've been told to not use methods that we haven't been taught in lesson, so to be safe, I'm not using it
     return biggest
