385070a35f5a26be3468262f3f0ce49fd9b93a3d
Incisivosaurus <drqonic@protonmail.com>
2024-11-14 15:12:39 +0000

    Added function to convert kilometers to miles

---
+++ BU-2024-LAB-6/km_to_miles.py
@@ -0,0 +1,10 @@
+def km_to_miles(kilometers):
+    miles = kilometers * 0.62137 # Convert kilometers to miles
+
+    return round(miles, 3) # Round to the nearest 3 decimals
+
+
+if __name__ == "__main__":
+    print(km_to_miles(1) == 0.621) # Expected result is True
+    print(km_to_miles(5) == 3.107) # Expected result is True
+    print(km_to_miles(7) == 4.349) # Expected result is False
